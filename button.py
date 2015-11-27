5395e78943679dd92e48e5b763b94a06768a79e1
GO+ <info@goplusplatform.com>
2015-11-27 14:47:09 +0300

    Update button.py

--- button.py
+++ button.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import mraa
@@ -11,20 +10,21 @@ URL_BASE_INC = "http://168.63.82.20/server/income/?did=%s&action=inc" % DID
 URL_BASE_DEC = "http://168.63.82.20/server/income/?did=%s&action=dec" % DID
 while True:
     # get action from some function
-    action = 'inc'
+    action = getAction()
+
     if action == 'inc':
         # send data to GO PLUS PLATFORM
         url = URL_BASE_INC
-        
+
     else if action == 'dec':
         # send data to GO PLUS PLATFORM
         url = URL_BASE_DEC
-        
+
     try:
         urllib2.urlopen(url)
     except urllib2.HTTPError as e:
         if e.code == '404':
             print("URL not found: %s" % url)
-    
+
     # sleep for 1 second
     time.sleep(1)
